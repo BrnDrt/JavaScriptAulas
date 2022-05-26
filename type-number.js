5d4f21a71f80421ba1861c3e3453cdbd22f7b1aa
Breno <breno.duarte98@gmail.com>
2022-05-26 19:34:40 -0300

    Exemplos das aulas da Alura

---
+++ type-number.js
@@ -0,0 +1,27 @@
+// Tipo Number
+
+const meuNumero = 3;
+
+const primeiroNumero = 1;
+const segundoNumero = 2;
+
+const operacaoMatematica = primeiroNumero - segundoNumero;
+
+console.log(operacaoMatematica)
+
+
+// Ponto Flutuante
+
+const numeroPontoFlutuante = 3.3;
+const pontoFlutuante = .5;
+
+const novaOperacao = primeiroNumero / numeroPontoFlutuante;
+
+console.log(novaOperacao)
+
+
+// NaN -> Not A Number (não é um número)
+
+const alura = 10/0;
+
+console.log(alura)
---
+++ type-string.js
@@ -0,0 +1,19 @@
+const texto1 = 'Olá, Mundo!';
+const texto2 = "Olá, Mundo!";
+
+const senha = 'senhaSuperSegura456!';
+
+const StringDeNumeros = '34567';
+
+const citacao = 'Meu nome é ';
+const meuNome = 'Breno'
+
+console.log(citacao)
+
+
+// concatenação (+)
+
+console.log(citacao + meuNome)
+
+//template string OU template literal
+
